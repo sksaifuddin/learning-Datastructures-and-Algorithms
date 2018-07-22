52165ffb98be550636fe52001e33370d14ae2725
sksaifuddin <atushaik@gmail.com>
2018-07-22 16:42:32 +0530

    question 2 solved

---
+++ InterviewQuestions/1.Arrays&Strings/question2/question2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+string reverseString(string word) {
+    cout <<"The reverse of the word is:"<<endl;
+    for( int i=word.length()-1;i>=0;i-- ){
+            cout<<word[i]<<endl;
+    }
+    return word;
+}
+
+int main() {
+    string word = "saif";
+    string result = reverseString(word);
+    return 0;
+}
