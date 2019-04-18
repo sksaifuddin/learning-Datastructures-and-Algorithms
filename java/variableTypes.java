25aa0c0695518143a780345a5c4f6a623e801de1
sksaifuddin <atushaik@gmail.com>
2019-04-18 17:40:50 +0530

    completed variable types

---
+++ java/variableTypes.java
@@ -0,0 +1,9 @@
+class Vartypes {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+        System.out.println(a+b);
+        char name = "saif";
+        System.out.println(name);
+  }
+}
