fae54c5f20bc055c12e046f1003401abd2526fd4
sksaifuddin <atushaik@gmail.com>
2019-04-20 21:34:20 +0530

    completed third pattern

---
+++ patterns/Pattern3.java
@@ -0,0 +1,28 @@
+// Pattern 3
+// *
+// **
+// ***
+// ****
+// ****
+// ***
+// **
+// *
+
+// basically its a combination of pattern 1 and 2
+
+class Pattern3 {
+    public static void main(String[] args) {
+        for( int i = 1; i <= 4; i++ ) {
+            for ( int j = 1 ; j <= i; j++ ) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        for( int i = 1; i <= 4; i++ ) {
+            for ( int j = 4 ; j >= i; j-- ) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
