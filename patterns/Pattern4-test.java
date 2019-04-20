18bfe0faefda78118453cd62310b58c1b2d35cc0
sksaifuddin <atushaik@gmail.com>
2019-04-21 01:49:30 +0530

    patter5 adeed

--- patterns/Pattern4-test.java
+++ patterns/Pattern4-test.java
@@ -2,19 +2,14 @@ import java.util.Scanner;
 class Pattern4Test {
     public static void main(String[] args) {
         int noOfCases =0;
+        int patternSize = 0;
         Scanner in = new Scanner(System.in);
         noOfCases = in.nextInt();
-        int casesArray[] = new int[noOfCases];
-        for(int i = 0; i < noOfCases; i++) {
-            casesArray[i] = in.nextInt();
-        }
-        // completed getting the inputs
-
-        for( int cases = 0; cases < casesArray.length; cases++ ) {
-            int sampleCase = casesArray[cases];
-            System.out.println("Case #" + (cases+1));
-            for( int i=1; i<=sampleCase;i++ ) {
-                for( int j=(sampleCase-1); j >= i ; j-- ) {
+        for( int cases = 1; cases <= noOfCases; cases++  ) {
+            patternSize = in.nextInt();
+            System.out.println("Case #"+ cases);
+            for( int i=1; i<= patternSize;i++ ) {
+                for( int j=patternSize-1; j >= i ; j-- ) {
                     System.out.print(" ");
                 }
                 for( int k=1; k<=i; k++ ) {
---
+++ patterns/Pattern5.java
@@ -0,0 +1,20 @@
+// Pattern 5
+//    *
+//   * *
+//  * * *
+// * * * *
+
+class Pattern5 {
+    public static void main(String[] args) {
+        for( int i=1; i<=4;i++) {
+            for(int j=4;j>=1;j--) {
+                if(j>i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(" *");                   
+                }
+            }
+            System.out.println();
+        }
+    }
+}
